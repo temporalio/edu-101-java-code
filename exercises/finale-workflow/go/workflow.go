524f1d9a2b9b3b939a0885427c7f79f80d6d471c
Tom Wheeler <tom@temporal.io>
2023-04-27 16:16:53 -0500

    added Go code for the Workflow Worker and starter

---
+++ exercises/finale-workflow/go/workflow.go
@@ -0,0 +1,29 @@
+package finale
+
+import (
+	"time"
+
+	"go.temporal.io/sdk/workflow"
+)
+
+func CertificateGeneratorWorkflow(ctx workflow.Context, name string) (string, error) {
+	logger := workflow.GetLogger(ctx)
+	logger.Info("Workflow started", "recipientName", name)
+
+	ao := workflow.ActivityOptions{
+		StartToCloseTimeout: 10 * time.Second,
+	}
+	ctx = workflow.WithActivityOptions(ctx, ao)
+
+	var outFilePath string
+	// NOTE: "CreatePdf" is the Activity Type defined in the implementation of the Java Activity code
+	err := workflow.ExecuteActivity(ctx, "CreatePdf", name).Get(ctx, &outFilePath)
+	if err != nil {
+		logger.Error("Activity failed.", "Error", err)
+		return "", err
+	}
+
+	logger.Info("CertificateGeneratorWorkflow created file", outFilePath)
+
+	return outFilePath, nil
+}
