e479a2ff34decc67f54f87a127fd53f326008ec1
Tom Wheeler <tom@temporal.io>
2023-05-10 12:05:55 -0500

    Update GreetingWorkflowImpl.java
    Reformat per Java conventions

--- exercises/farewell-workflow/solution/src/main/java/farewellworkflow/GreetingWorkflowImpl.java
+++ exercises/farewell-workflow/solution/src/main/java/farewellworkflow/GreetingWorkflowImpl.java
@@ -8,15 +8,15 @@ import java.time.Duration;
 public class GreetingWorkflowImpl implements GreetingWorkflow {
 
     ActivityOptions options = ActivityOptions.newBuilder()
-        .setStartToCloseTimeout(Duration.ofSeconds(5))
-        .build();
+            .setStartToCloseTimeout(Duration.ofSeconds(5))
+            .build();
 
     private final GreetingActivities activities = Workflow.newActivityStub(GreetingActivities.class, options);
 
     @Override
-    public String greetSomeone(String name){
+    public String greetSomeone(String name) {
         String spanishGreeting = activities.greetInSpanish(name);
-        String spanishFarewell = activities.farewellInSpanish(name);      
+        String spanishFarewell = activities.farewellInSpanish(name);
 
         return "\n" + spanishGreeting + "\n" + spanishFarewell;
     }
