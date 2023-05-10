23c48ba95f71438c722f1f9d6ee434bf5fff14d9
Mason Egger <mason@masonegger.com>
2023-05-10 13:02:20 -0500

    updating exception handling in the Activity and a few README mistakes (#4)
    * updating exception handling in the Activity and a few README mistakes
    
    * Update GreetingActivitiesImpl.java
    
    reformat based on Java language conventions
    
    * Update GreetingActivitiesImpl.java
    
    reformatted as per Java conventions
    
    * Update GreetingWorkflowImpl.java
    
    Reformat per Java conventions
    
    ---------
    
    Co-authored-by: Tom Wheeler <tom@temporal.io>

--- exercises/farewell-workflow/practice/src/main/java/farewellworkflow/GreetingActivitiesImpl.java
+++ exercises/farewell-workflow/practice/src/main/java/farewellworkflow/GreetingActivitiesImpl.java
@@ -1,53 +1,48 @@
 package farewellworkflow;
 
-import org.apache.commons.lang3.StringEscapeUtils;
-import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.IOException;
+import io.temporal.activity.Activity;
 
 public class GreetingActivitiesImpl implements GreetingActivities {
 
-    public String greetInSpanish(String name){
+    @Override
+    public String greetInSpanish(String name) {
         return callService("get-spanish-greeting", name);
     }
 
     /* TODO: Implement the Activity method that was defined in the
-     * GreetingActivities interface. This method should call the microservice to
-     * get a farewell message in Spanish. It will be identical to the
-     * method above, except the first argument to the callService
+     * GreetingActivities interface. This method should call the microservice 
+     * to get a farewell message in Spanish. It will be identical to the
+     * method above, except that the first argument to the callService
      * method will be "get-spanish-farewell". The name of method needs to 
      * be the same as in the GreetingActivities interface
-    */
-    
-    // utility method for making calls to the microservices
-    String callService(String stem, String name){
+     */
+    String callService(String stem, String name) {
 
-        StringBuilder sb = new StringBuilder();
-        String baseUrl = String.format("http://localhost:9999/%s?name=%s", stem, StringEscapeUtils.escapeHtml4(name));
+        StringBuilder builder = new StringBuilder();
 
+        String baseUrl = "http://localhost:9999/%s?name=%s";
+
+        URL url = null;
         try {
-            URL url = new URL(baseUrl);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            conn.setRequestProperty("Content-Type", "application/json");
-
-            if (conn.getResponseCode() != 200) {
-                throw new RuntimeException("HTTP error code : " + conn.getResponseCode());
-            }
-
-            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
-
-            String output;
-            while ((output = br.readLine()) != null) {
-                sb.append(output);
-            }
-
-            conn.disconnect();
-        } catch (Exception e) {
-            e.printStackTrace();
+            url = new URL(String.format(baseUrl, stem, URLEncoder.encode(name, "UTF-8")));
+        } catch (IOException e) {
+            throw Activity.wrap(e);
         }
 
-        return sb.toString();
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()))) {
+            String line;
+            while ((line = in.readLine()) != null) {
+                builder.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            throw Activity.wrap(e);
+        }
+
+        return builder.toString();
     }
 }
--- exercises/farewell-workflow/solution/src/main/java/farewellworkflow/GreetingWorkflowImpl.java
+++ exercises/farewell-workflow/solution/src/main/java/farewellworkflow/GreetingWorkflowImpl.java
@@ -8,13 +8,13 @@ import java.time.Duration;
 public class GreetingWorkflowImpl implements GreetingWorkflow {
 
     ActivityOptions options = ActivityOptions.newBuilder()
-        .setStartToCloseTimeout(Duration.ofSeconds(5))
-        .build();
+            .setStartToCloseTimeout(Duration.ofSeconds(5))
+            .build();
 
     private final GreetingActivities activities = Workflow.newActivityStub(GreetingActivities.class, options);
 
     @Override
-    public String greetSomeone(String name){
+    public String greetSomeone(String name) {
         String spanishGreeting = activities.greetInSpanish(name);
         String spanishFarewell = activities.farewellInSpanish(name);
 
